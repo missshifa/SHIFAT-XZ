798077c06ebbbd971ce6ddb1c4c8b4b642169b1e
MD SHIFAT <kakasihatak8@gmail.com>
2025-10-03 10:12:58 +0600

    Update onEvent.js

--- scripts/events/onEvent.js
+++ scripts/events/onEvent.js
@@ -12,7 +12,7 @@ module.exports = {
 	onStart: async ({ api, args, message, event, threadsData, usersData, dashBoardData, threadModel, userModel, dashBoardModel, role, commandName }) => {
 		for (const item of allOnEvent) {
 			if (typeof item === "string")
-				continue; // Skip if item is string, because it is the command name and is executed at ../../bot/handler/handlerEvents.js
+				continue; // Skip if item is string , because it is the command name and is executed at ../../bot/handler/handlerEvents.js
 			item.onStart({ api, args, message, event, threadsData, usersData, threadModel, dashBoardData, userModel, dashBoardModel, role, commandName });
 		}
 	}
