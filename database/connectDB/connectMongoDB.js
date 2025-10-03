7d02b03cb727355b9dfd1768caa82aff16ea2be7
MD SHIFAT <kakasihatak8@gmail.com>
2025-10-03 08:55:49 +0600

    Update connectMongoDB.js

--- database/connectDB/connectMongoDB.js
+++ database/connectDB/connectMongoDB.js
@@ -1,5 +1,5 @@
 module.exports = async function (uriConnect) {
-	const mongoose = require("mongoose"); 
+	const mongoose = require("mongoose");
 
 	const threadModel = require("../models/mongodb/thread.js");
 	const userModel = require("../models/mongodb/user.js");
