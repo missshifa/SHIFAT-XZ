431e2ef13d4e49d29966c062c24bf2bf81dccc80
MD SHIFAT <kakasihatak8@gmail.com>
2025-10-03 10:35:07 +0600

    Update avatar.js

--- scripts/cmds/avatar.js
+++ scripts/cmds/avatar.js
@@ -4,7 +4,7 @@ const { getStreamFromURL } = global.utils;
 module.exports = {
 	config: {
 		name: "avatar",
-		author: "NTKhang",
+		author: "NTKhang / SHIFAT",
 		version: "1.6",
 		cooldowns: 5,
 		role: 0,
