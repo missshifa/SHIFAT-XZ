cb268ffcafca3daf092572aa2e2d57eb8a62a679
MD SHIFAT <kakasihatak8@gmail.com>
2025-10-03 10:17:02 +0600

    Update adboxonly.js

--- scripts/cmds/adboxonly.js
+++ scripts/cmds/adboxonly.js
@@ -1,7 +1,7 @@
 module.exports = {
 	config: {
 		name: "onlyadminbox",
-		aliases: ["onlyadbox", "adboxonly", "adminboxonly"],
+		aliases: ["onlyadbox", "adonly", "adminboxonly"],
 		version: "1.3",
 		author: "NTKhang",
 		countDown: 5,
