93be1410598014db4b6a0a0035b9881862dd22bf
MD SHIFAT <kakasihatak8@gmail.com>
2025-10-03 08:37:07 +0600

    Update custom.js

--- bot/custom.js
+++ bot/custom.js
@@ -2,7 +2,7 @@ const { log } = global.utils;
 
 module.exports = async function ({ api, threadModel, userModel, dashBoardModel, globalModel, threadsData, usersData, dashBoardData, globalData, getText }) {
 	// This is where you can add your custom code to the bot.
-	// The bot will run this code every time it starts up (after logging in and loading data from the database).
+	// The bot will run this code  every time it starts up (after logging in and loading data from the database).
 
 	setInterval(async () => {
 		api.refreshFb_dtsg()
